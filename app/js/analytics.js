07871388a52c8a2def0a787c34a8bc9514c0a2ad
ewhitebloom <ewhitebloom@gmail.com>
2014-06-01 12:14:15 -0400

    major logic changes to word suggestions, formatting and more to analytics.js

--- app/js/analytics.js
+++ app/js/analytics.js
@@ -4,25 +4,31 @@ var topcharactersbutton = document.getElementById('topcharacters_button');
 
 var input_area = document.getElementById('words_textarea');
 
-countbutton.onclick=function(){
+countbutton.onclick = function() {
   wordCount();
-  input_area.onkeydown=function(){ wordCount() }
+  input_area.onkeydown = function() {
+    wordCount()
+  }
 };
 
-topwordsbutton.onclick=function(){
+topwordsbutton.onclick = function() {
   topWords();
-  input_area.onkeydown=function(){ topWords() }
+  input_area.onkeydown = function() {
+    topWords()
+  }
 };
 
-topcharactersbutton.onclick=function(){
+topcharactersbutton.onclick = function() {
   topcharacters();
-  input_area.onkeydown=function(){ topcharacters() }
+  input_area.onkeydown = function() {
+    topcharacters()
+  }
 };
 
 
 function wordCount() {
 
-  document.getElementById('all_text').innerHTML='';
+  document.getElementById('all_text').innerHTML = '';
 
   var words = document.getElementById("words_textarea").value.trim().split(' ');
 
@@ -45,17 +51,17 @@ function wordCount() {
 
 function topWords() {
 
-  document.getElementById('all_text').innerHTML='';
+  document.getElementById('all_text').innerHTML = '';
 
   var words = document.getElementById("words_textarea").value.trim().split(' ');
 
   var counts = [];
 
-  for (i=0;i<=words.length - 1;i++) {
+  for (i = 0; i <= words.length - 1; i++) {
     var found = false;
     var current_word = words[i].toLowerCase();
 
-    for (n=0;n<=counts.length - 1;n++) {
+    for (n = 0; n <= counts.length - 1; n++) {
       if (counts[n][0] == current_word) {
         counts[n][1] += 1;
         found = true;
@@ -64,32 +70,32 @@ function topWords() {
     }
 
     if (found == false) {
-      counts.push([current_word,1]);
+      counts.push([current_word, 1]);
     }
   }
 
-  var sorted = counts.sort(function(a,b) {
+  var sorted = counts.sort(function(a, b) {
     return b[1] - a[1]
   });
 
   function createTable() {
     var element = document.getElementById("all_text");
 
-    var tbl=document.createElement('table');
+    var tbl = document.createElement('table');
 
-    tbl.style.width='100%';
+    tbl.style.width = '100%';
 
-    tbl.setAttribute('border','1');
+    tbl.setAttribute('border', '1');
 
-    var tbdy=document.createElement('tbody');
+    var tbdy = document.createElement('tbody');
 
-    var tr=document.createElement('tr');
+    var tr = document.createElement('tr');
     var word = document.createElement('td');
-    word.innerHTML="<strong>Word</strong>"
+    word.innerHTML = "<strong>Word</strong>"
     var countword = document.createElement('td');
-    countword.innerHTML="<strong>Count</strong>"
+    countword.innerHTML = "<strong>Count</strong>"
     var suggestions = document.createElement('td');
-    suggestions.innerHTML="<strong>Suggestions</strong>"
+    suggestions.innerHTML = "<strong>Suggestions</strong>"
     tr.appendChild(word)
     tr.appendChild(countword)
     tr.appendChild(suggestions)
@@ -97,43 +103,85 @@ function topWords() {
     tbl.appendChild(tbdy);
     element.appendChild(tbl)
 
-    for(i=0;i<=counts.length - 1;i++) {
-      var tr=document.createElement('tr');
+    for (i = 0; i <= counts.length - 1; i++) {
+      var tr = document.createElement('tr');
       var word = document.createElement('td');
-      word.innerHTML=counts[i][0]
+      word.innerHTML = counts[i][0]
       var countword = document.createElement('td');
-      countword.innerHTML=counts[i][1]
+      countword.innerHTML = counts[i][1]
 
       if (counts[i][1] >= 3) {
         var suggestion = document.createElement('td');
-        $.getJSON('http://words.bighugelabs.com/api/2/dafe2e8acd88d00e5096b17ca16157a7/' + counts[i][0] + '/json', {}, function(data) {
-          suggestion.innerHTML= 'data[0][0] + data[0][1]'
+        $.ajax({
+          type: 'GET',
+          url: 'http://words.bighugelabs.com/api/2/dafe2e8acd88d00e5096b17ca16157a7/' + counts[i][0] + '/json',
+          dataType: 'jsonp',
+          crossDomain: true,
+          success: function(data) {
+            debugger;
+            if (typeof data.noun != "undefined") {
+              if (typeof data.noun.syn != "undefined" && typeof data.noun.sim === "undefined") {
+                suggestion.innerHTML = "noun: <em>synonyms</em> " + data.noun.syn.slice(0, 5)
+              } else if (typeof data.noun.syn === "undefined" && typeof data.noun.sim != "undefined") {
+                suggestion.innerHTML = "noun: <em>similar</em> " + data.noun.sim.slice(0, 5)
+              } else if (typeof data.noun.syn != "undefined" && typeof data.noun.sim != "undefined") {
+                suggestion.innerHTML = "noun: <em>synonyms</em> " + data.noun.syn.slice(0, 5) + " <em>similar</em> " + data.noun.sim.slice(0, 5)
+              }
+            } else if (typeof data.verb != "undefined") {
+              if (typeof data.verb.syn != "undefined" && typeof data.verb.sim === "undefined") {
+                suggestion.innerHTML = suggestion.innerHTML + "verb: <em>synonyms</em> " + data.verb.syn.slice(0, 5)
+              } else if (typeof data.verb.syn === "undefined" && typeof data.verb.sim != "undefined") {
+                suggestion.innerHTML = suggestion.innerHTML + "verb: <em>similar</em> " + data.verb.sim.slice(0, 5)
+              } else if (typeof data.verb.syn != "undefined" && typeof data.verb.sim != "undefined") {
+                suggestion.innerHTM L = suggestion.innerHTML + "verb: <em>synonyms</em> " + data.verb.syn.slice(0, 5) + " <em>similar</em> " + data.verb.sim.slice(0, 5)
+              }
+            } else if (typeof data.adverb != "undefined") {
+              if (typeof data.adverb.syn != "undefined" && typeof data.adverb.sim === "undefined") {
+                suggestion.innerHTML = suggestion.innerHTML + "adverb: <em>synonyms</em> " + data.adverb.syn.slice(0, 5)
+              } else if (typeof data.adverb.syn === "undefined" && typeof data.adverb.sim != "undefined") {
+                suggestion.innerHTML = suggestion.innerHTML + "adverb: <em>similar</em> " + data.adverb.sim.slice(0, 5)
+              } else if (typeof data.adverb.syn != "undefined" && typeof data.adverb.sim != "undefined") {
+                suggestion.innerHTM L = suggestion.innerHTML + "adverb: <em>synonyms</em> " + data.adverb.syn.slice(0, 5) + " <em>similar</em> " + data.adverb.sim.slice(0, 5)
+              }
+            } else if (typeof data.adjective != "undefined") {
+              if (typeof data.adjective.syn != "undefined" && typeof data.adjective.sim === "undefined") {
+                suggestion.innerHTML = suggestion.innerHTML + "adjective: <em>synonyms</em> " + data.adjective.syn.slice(0, 5)
+              } else if (typeof data.adjective.syn === "undefined" && typeof data.adjective.sim != "undefined") {
+                suggestion.innerHTML = suggestion.innerHTML + "adjective: <em>similar</em> " + data.adjective.sim.slice(0, 5)
+              } else if (typeof data.adjective.syn != "undefined" && typeof data.adjective.sim != "undefined") {
+                suggestion.innerHTM L = suggestion.innerHTML + "adjective: <em>synonyms</em> " + data.adjective.syn.slice(0, 5) + " <em>similar</em> " + data.adjective.sim.slice(0, 5)
+              }
+            }
+            debugger;
+          },
+          error: function(request, status, error) {
+            console.log(error);
+          }
         });
       }
-
       tr.appendChild(word)
       tr.appendChild(countword)
       tr.appendChild(suggestion)
-      tbdy.appendChild(tr);
+      tbdy.appendChild(tr)
     }
-    tbl.appendChild(tbdy);
+    tbl.appendChild(tbdy)
     element.appendChild(tbl)
   }
   createTable()
-}
+};
 
 function topcharacters() {
-  document.getElementById('all_text').innerHTML='';
+  document.getElementById('all_text').innerHTML = '';
 
   var characters = document.getElementById("words_textarea").value.trim().split('');
 
   var counts = [];
 
-  for (i=0;i<=characters.length - 1;i++) {
+  for (i = 0; i <= characters.length - 1; i++) {
     var found = false;
     var current_letter = characters[i].toLowerCase();
 
-    for (n=0;n<=counts.length - 1;n++) {
+    for (n = 0; n <= counts.length - 1; n++) {
       if (counts[n][0] == current_letter) {
         counts[n][1] += 1;
         found = true;
@@ -142,42 +190,42 @@ function topcharacters() {
     }
 
     if (found == false) {
-      counts.push([current_letter,1]);
+      counts.push([current_letter, 1]);
     }
   }
 
-  var sorted = counts.sort(function(a,b) {
+  var sorted = counts.sort(function(a, b) {
     return b[1] - a[1]
   });
 
   function createTable() {
     var element = document.getElementById("all_text");
 
-    var tbl=document.createElement('table');
+    var tbl = document.createElement('table');
 
-    tbl.style.width='100%';
+    tbl.style.width = '100%';
 
-    tbl.setAttribute('border','1');
+    tbl.setAttribute('border', '1');
 
-    var tbdy=document.createElement('tbody');
+    var tbdy = document.createElement('tbody');
 
-    var tr=document.createElement('tr');
+    var tr = document.createElement('tr');
     var word = document.createElement('td');
-    word.innerHTML="<strong>Character</strong>"
+    word.innerHTML = "<strong>Character</strong>"
     var countword = document.createElement('td');
-    countword.innerHTML="<strong>Count</strong>"
+    countword.innerHTML = "<strong>Count</strong>"
     tr.appendChild(word)
     tr.appendChild(countword)
     tbdy.appendChild(tr);
     tbl.appendChild(tbdy);
     element.appendChild(tbl)
 
-    for(i=0;i<=counts.length - 1;i++){
-      var tr=document.createElement('tr');
+    for (i = 0; i <= counts.length - 1; i++) {
+      var tr = document.createElement('tr');
       var word = document.createElement('td');
-      word.innerHTML=counts[i][0]
+      word.innerHTML = counts[i][0]
       var countword = document.createElement('td');
-      countword.innerHTML=counts[i][1]
+      countword.innerHTML = counts[i][1]
       tr.appendChild(word)
       tr.appendChild(countword)
       tbdy.appendChild(tr);
