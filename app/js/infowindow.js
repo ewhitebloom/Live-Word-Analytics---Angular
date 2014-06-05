372a5cdf4bea33a6f6cc403284a0271b3c3eadf2
ewhitebloom <ewhitebloom@gmail.com>
2014-06-05 19:12:20 -0400

    styling

--- app/js/infowindow.js
+++ app/js/infowindow.js
@@ -1,13 +1,13 @@
-  $("#infowindow").mouseover(function() {
-    var headline = document.getElementById('headline');
-    var infobox = document.createElement('p');
-    infobox.setAttribute('id','infotext')
-    infobox.style.display = 'block'
-    infobox.style.background = '#e4e4e4'
-    infobox.innerHTML = "Click on 'Top Words' to toggle a live-updating table of each word's count, along with thesarus suggestions for word counts above 3."
-    headline.appendChild(infobox)
-  });
+$("#infowindow").mouseover(function() {
+  var headline = document.getElementById('headline');
+  var infobox = document.createElement('p');
+  infobox.setAttribute('id','infotext')
+  infobox.style.display = 'block'
+  infobox.style.background = '#e4e4e4'
+  infobox.innerHTML = "Click on 'Top Words' to toggle a live-updating table of each word's count, along with thesarus suggestions for word counts above 3."
+  headline.appendChild(infobox)
+});
 
-  $("#infowindow").mouseleave(function() {
-    $('#infotext').remove();
-  });
+$("#infowindow").mouseleave(function() {
+  $('#infotext').remove();
+});
