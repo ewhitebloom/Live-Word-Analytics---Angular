3e456fe0ebcd762f19c216ad3083114d0f89c707
ewhitebloom <ewhitebloom@gmail.com>
2014-06-05 18:12:13 -0400

    styling

--- app/js/infowindow.js
+++ app/js/infowindow.js
@@ -3,7 +3,7 @@
     var infobox = document.createElement('p');
     infobox.setAttribute('id','infotext')
     infobox.style.display = 'block'
-    infobox.style.color = 'green'
+    infobox.style.background = '#e4e4e4'
     infobox.innerHTML = "Click on 'Top Words' to toggle a live-updating table of each word's count, along with thesarus suggestions for counts above 3."
     headline.appendChild(infobox)
   });
